9a38d01a406695f75a6039b52f4bed4da4048a33
Hardydev <dusgh15378@gmail.com>
2023-03-13 11:39:24 +0900

    :sparkle: 앱 메인 화면 변경

--- JBNU_BukBob/JBNU_BukBob/JBNU_BukBobApp.swift
+++ JBNU_BukBob/JBNU_BukBob/JBNU_BukBobApp.swift
@@ -13,8 +13,9 @@ struct JBNU_BukBobApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainView()
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
